7991f0371fcb04fba42eff253ba5cffb22966960
LambertWu <26795806@qq.com>
2018-01-22 23:53:54 +0800

    Add menu controller

---
+++ BackEnd/AspNetCore.Api/Controllers/MenusController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using AspNetCore.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.Api.Controllers
+{
+    /// <summary>
+    /// 菜单控制器
+    /// </summary>
+    [Produces("application/json")]
+    [Route("api/Menus")]
+    public class MenusController : Controller
+    {
+        /// <summary>
+        /// 获取菜单列表
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(Menu), (int)HttpStatusCode.OK)]
+        public IActionResult GetMenus()
+        {
+            var menu = new Menu
+            {
+                Text = "主导航",
+                Group = true,
+                Children = new List<Menu>
+                {
+                    new Menu
+                    {
+                        Text = "仪表盘",
+                        Link = "/dashboard",
+                        Icon = "icon-speedometer"
+                    },
+                    new Menu
+                    {
+                        Text = "快捷菜单",
+                        Icon = "icon-rocket",
+                        Shortcut_root = true
+                    },
+                    new Menu
+                    {
+                        Text = "测试",
+                        Link = "/apiValues",
+                        Icon = "icon-rocket"
+                    },
+                    new Menu
+                    {
+                        Text = "产品",
+                        Link = "/products/list",
+                        Icon = "icon-rocket"
+                    }
+                }
+            };
+
+            return Ok(menu);
+        }
+    }
+}
---
+++ BackEnd/AspNetCore.Api/Models/Menu.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Api.Models
+{
+    /// <summary>
+    /// 菜单项
+    /// </summary>
+    public class Menu
+    {
+        /// <summary>
+        /// 文本
+        /// </summary>
+        public string Text { get; set; }
+
+        /// <summary>
+        /// i18n主键
+        /// </summary>
+        public string I18n { get; set; }
+
+        /// <summary>
+        /// 是否菜单组
+        /// </summary>
+        public bool? Group { get; set; }
+
+        /// <summary>
+        /// angular 路由
+        /// </summary>
+        public string Link { get; set; }
+
+        /// <summary>
+        /// 外部链接
+        /// </summary>
+        public string ExternalLink { get; set; }
+
+        /// <summary>
+        /// 链接 Target
+        /// </summary>
+        public string Target { get; set; }
+
+        /// <summary>
+        /// 图标
+        /// </summary>
+        public string Icon { get; set; }
+
+        /// <summary>
+        /// 徽标数，展示的数字。（注：`group:true` 无效）
+        /// </summary>
+        public string Badge { get; set; }
+
+        /// <summary>
+        /// 徽标数，显示小红点
+        /// </summary>
+        public bool? Badge_dot { get; set; }
+
+        /// <summary>
+        /// 徽标数，设置 Badge 颜色
+        /// </summary>
+        public string Badge_status { get; set; }
+
+        /// <summary>
+        /// 是否隐藏
+        /// </summary>
+        public bool? Hide { get; set; }
+
+        /// <summary>
+        /// TODO ACL配置 
+        /// </summary>
+        public string Acl { get; set; }
+
+        /// <summary>
+        /// 是否快捷菜单项
+        /// </summary>
+        public bool? Shortcut { get; set; }
+
+        /// <summary>
+        /// 快捷菜单根节点
+        /// </summary>
+        public bool? Shortcut_root { get; set; }
+
+        /// <summary>
+        /// 是否允许服用
+        /// </summary>
+        public bool? Reuse { get; set; }
+
+        /// <summary>
+        /// 二级菜单
+        /// </summary>
+        public ICollection<Menu> Children { get; set; }
+    }
+}
